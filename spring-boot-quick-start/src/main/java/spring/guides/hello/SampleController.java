e34655c6d68e208b62504e1abcb03bec7b1bf0e6
Edward Lee <bert825_work@163.com>
2017-02-07 08:58:19 +0800

    Quick Start of Spring Boot

---
+++ spring-boot-quick-start/src/main/java/spring/guides/hello/SampleController.java
@@ -0,0 +1,27 @@
+package spring.guides.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 示例控制器及入口点。
+ *
+ * @author dannong
+ * @since 2017年02月07日 08:38
+ */
+@RestController("sampleController") // = @Controller + @ResponseBody
+@SpringBootApplication // = @SpringBootConfiguration/@Configuration + @EnableAutoConfiguration + @ComponentScan
+public class SampleController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleController.class, args);
+    }
+
+}
