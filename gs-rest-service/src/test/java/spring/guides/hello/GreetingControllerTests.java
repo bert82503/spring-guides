199a2fb0178697ff2d997064658d11b3dfa5b6be
Edward Lee <bert825_work@163.com>
2017-02-22 14:40:03 +0800

    [gs-rest-service] add test of GreetingController

---
+++ gs-rest-service/src/test/java/spring/guides/hello/GreetingControllerTests.java
@@ -0,0 +1,47 @@
+package spring.guides.hello;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Test of {@link GreetingController}.
+ *
+ * @author dannong
+ * @since 2017年02月21日 23:59
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GreetingControllerTests {
+
+    @Resource
+    private MockMvc mockMvc;
+
+    @Test
+    public void noParam() throws Exception {
+        this.mockMvc.perform(get("/greeting"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
+
+    @Test
+    public void param() throws Exception {
+        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+    }
+
+}
