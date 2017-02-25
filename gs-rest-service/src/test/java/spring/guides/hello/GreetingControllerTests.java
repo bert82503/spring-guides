532756f941837f19e396976e89bddc2e5990fb2e
Edward Lee <bert825_work@163.com>
2017-02-25 22:11:05 +0800

    [gs-rest-service] optimize Integration Test (AbstractIntegrationTests)

--- gs-rest-service/src/test/java/spring/guides/hello/GreetingControllerTests.java
+++ gs-rest-service/src/test/java/spring/guides/hello/GreetingControllerTests.java
@@ -5,14 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import javax.annotation.Resource;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * Test of {@link GreetingController}.
@@ -20,13 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
  * @author dannong
  * @since 2017年02月21日 23:59
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class GreetingControllerTests {
-
-    @Resource
-    private MockMvc mockMvc;
+public class GreetingControllerTests extends AbstractIntegrationTests {
 
     @Test
     public void noParam() throws Exception {
