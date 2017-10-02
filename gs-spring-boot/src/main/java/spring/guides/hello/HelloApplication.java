833688d6da6b341c41d7de1550c9fb3e48746f4e
Edward Lee <bert825_work@163.com>
2017-10-03 01:23:53 +0800

    'Spring Boot 提供的组件'剖析

--- gs-spring-boot/src/main/java/spring/guides/hello/HelloApplication.java
+++ gs-spring-boot/src/main/java/spring/guides/hello/HelloApplication.java
@@ -1,7 +1,5 @@
 package spring.guides.hello;
 
-import java.util.Arrays;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -41,7 +39,7 @@ public class HelloApplication {
 //      System.out.println("Let's inspect the beans provided by Spring Boot:");
 
       String[] beanNames = ctx.getBeanDefinitionNames();
-      Arrays.sort(beanNames);
+//      Arrays.sort(beanNames);
       for (String beanName : beanNames) {
         logger.info(beanName);
       }
