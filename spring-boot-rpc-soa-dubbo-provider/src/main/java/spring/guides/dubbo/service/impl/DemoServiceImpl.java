bac8c23a66de6c01e10ad6f47785dd1b9308c96d
Edward Lee <bert825_work@163.com>
2017-10-04 20:02:55 +0800

    [RPC SOA] Dubbo Service Provider

---
+++ spring-boot-rpc-soa-dubbo-provider/src/main/java/spring/guides/dubbo/service/impl/DemoServiceImpl.java
@@ -0,0 +1,23 @@
+package spring.guides.dubbo.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import spring.guides.dubbo.service.DemoService;
+
+/**
+ * 演示服务实现。
+ *
+ * @author dannong
+ * @since 2017年04月05日 11:45
+ */
+@Service("demoService")
+public class DemoServiceImpl implements DemoService {
+
+  private static final String TEMPLATE = "Hello, %s!";
+
+  @Override
+  public String sayHello(String name) {
+    return String.format(TEMPLATE, name);
+  }
+
+}
