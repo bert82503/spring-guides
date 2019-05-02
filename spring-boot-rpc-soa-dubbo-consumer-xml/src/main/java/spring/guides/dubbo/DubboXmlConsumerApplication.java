ade28ee806c0b369ad818088791e4602583f832d
Edward Lee <bert825_work@163.com>
2019-05-02 16:58:04 +0800

    Upgrade to spring-boot-starter-parent 1.5.19.RELEASE

--- spring-boot-rpc-soa-dubbo-consumer-xml/src/main/java/spring/guides/dubbo/DubboXmlConsumerApplication.java
+++ spring-boot-rpc-soa-dubbo-consumer-xml/src/main/java/spring/guides/dubbo/DubboXmlConsumerApplication.java
@@ -1,6 +1,5 @@
 package spring.guides.dubbo;
 
-import io.dubbo.springboot.DubboAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -13,10 +12,7 @@ import org.springframework.context.annotation.ImportResource;
  * @author dannong
  * @since 2017年04月05日 22:22
  */
-@SpringBootApplication(
-    exclude = {DubboAutoConfiguration.class,
-        DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class}
-)
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 @ImportResource({"classpath*:META-INF/spring/*.xml"})
 @SuppressWarnings("startup-enter-point")
 public class DubboXmlConsumerApplication {
